5296a5346fc3ebf993e7c5264d5dec15e683afc0
Aleksander Jaworski <aleck156@gmail.com>
2021-09-14 21:09:26 +0200

    add more fields into object

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const app = Vue.createApp({
   data() {
     return {
       firstName: 'John',
+      lastName: 'Doe',
+      email: 'gmail@johnDoe.com',
+      gender: 'male',
+      picture: 'https://randomuser.me/api/portraits/men/1-/jpg',
     };
   },
 });
